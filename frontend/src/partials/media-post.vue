5343a93bd5436531a387c03ea6d3c438b5b94279
AndreMor <me@andremor.dev>
2022-09-29 07:46:11 -0500

    fixing wm page

--- frontend/src/partials/media-post.vue
+++ frontend/src/partials/media-post.vue
@@ -1,8 +1,8 @@
 <template>
-  <section class="hero" :class="'type-' + type" style="padding: 12px">
+  <section class="hero" :class="'type-' + type">
     <div class="hero-head" style="text-align: left">
-      <strong style="position: absolute">{{ showType }}</strong>
-      <div v-if="admin || userID === loggedID" style="position: absolute; right: 12px" class="buttons">
+      <strong class="wm-show-type">{{ showType }}</strong>
+      <div v-if="admin || userID === loggedID" class="buttons wm-but-r">
         <router-link :to="'/wubbzy-media/publish/' + _id" class="button is-link is-responsive">
           Edit post
         </router-link>
@@ -33,11 +33,25 @@
 <style scoped>
 .hero {
   border-radius: 1rem;
+  padding: 12px;
+}
+
+.buttons {
+  justify-content: flex-start;
+}
+
+.wm-show-type {
+  position: absolute;
+}
+
+.wm-but-r {
+  position: absolute;
+  right: 28px
 }
 </style>
 
 <script>
-Array.apply();
+
 export default {
   props: ["_id", "userID", "title", "description", "mirrors", "type", "admin"],
   data() {
